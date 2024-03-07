cacbb050824db387a00ef224a26c9c756db14993
Lia Kahn <lia.j.kahn@gmail.com>
2024-03-07 15:51:43 -0700

    updated CLI interface

--- index.js
+++ index.js
@@ -16,6 +16,36 @@ const manager = new Manager('Joanne', 6, 'joanne@gmail.com', 101)
 //create an instance of the employee management system
 const empSystem = new EmployeeManagementSystem();
 
+
+//function to add an employee
+const addEmployee = (name, id, email) => {
+    const newEmp = new Employee(name, id, email);
+    empSystem.addEmployee(newEmp);
+
+}
+
+
+// function to remove an employee
+const removeEmployee = (id) => {
+    empSystem.removeEmployee(id);
+}
+
+// check if command-line arguments are provided
+if (process.argv.length >= 6 && process.argv[5] === 'remove') {
+    const id = parseInt(process.argv[2]);
+    removeEmployee(id);
+} else if (process.argv.length >= 5) {
+    const name = process.argv[2];
+    const id = parseInt(process.argv[3]);
+    const email = process.argv[4];
+    addEmployee(name, id, email);
+} else {
+       console.log("Usage:");
+    console.log("To add an employee: node index.js addEmployee <name> <id> <email>");
+    console.log("To remove an employee: node index.js removeEmployee <id>");
+}
+
+
 //add employees and managers to the employee management system
 empSystem.addEmployee(emp1);
 empSystem.addEmployee(emp2);
